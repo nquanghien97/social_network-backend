236aecd795c0977f6e53528dba0187b75dd147ce
nquanghien97 <nquanghien97@gmail.com>
2023-12-02 16:31:57 +0700

    fix deploy

--- src/index.ts
+++ src/index.ts
@@ -18,23 +18,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const allowCors = (fn: any) => async (req: Request, res: any) => {
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     // another common pattern
-//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//     )
-//     if (req.method === 'OPTIONS') {
-//       res.status(200).end()
-//       return
-//     }
-//     return await fn(req, res)
-//   }
-
 app.use('/api/auth', authRouter);
 
 app.use('/api', userRouter);
